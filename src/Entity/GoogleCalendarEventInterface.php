6676ab251406af3993d0b2aded4c75877345ccb2
Ruth Ivimey-Cook <ruth@ivimey.org>
2019-04-09 19:40:14 +0100

    Additional properties for events

--- src/Entity/GoogleCalendarEventInterface.php
+++ src/Entity/GoogleCalendarEventInterface.php
@@ -3,8 +3,8 @@
 namespace Drupal\google_calendar\Entity;
 
 use Drupal\Core\Entity\ContentEntityInterface;
-use Drupal\Core\Entity\RevisionLogInterface;
 use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\Core\Url;
 use Drupal\user\EntityOwnerInterface;
 
 /**
@@ -13,8 +13,6 @@ use Drupal\user\EntityOwnerInterface;
  * @ingroup google_calendar
  */
 interface GoogleCalendarEventInterface extends ContentEntityInterface, EntityChangedInterface, EntityOwnerInterface {
-
-  // Add get/set methods for your configuration properties here.
 
   /**
    * Gets the Google Calendar Event name.
@@ -33,7 +31,27 @@ interface GoogleCalendarEventInterface extends ContentEntityInterface, EntityCha
    * @return \Drupal\google_calendar\Entity\GoogleCalendarEventInterface
    *   The called Google Calendar Event entity.
    */
-  public function setName($name);
+//  public function setName($name): GoogleCalendarEventInterface;
+
+
+  /**
+   * Gets the Google Calendar Event description.
+   *
+   * @return string
+   *   Description of the Google Calendar Event.
+   */
+  public function getDescription();
+
+  /**
+   * Sets the Google Calendar Event description.
+   *
+   * @param string $description
+   *   The Google Calendar Event description.
+   *
+   * @return \Drupal\google_calendar\Entity\GoogleCalendarEventInterface
+   *   The called Google Calendar Event entity.
+   */
+//  public function setDescription($description): GoogleCalendarEventInterface;
 
   /**
    * Gets the Google Calendar Event creation timestamp.
@@ -52,8 +70,99 @@ interface GoogleCalendarEventInterface extends ContentEntityInterface, EntityCha
    * @return \Drupal\google_calendar\Entity\GoogleCalendarEventInterface
    *   The called Google Calendar Event entity.
    */
-  public function setCreatedTime($timestamp);
+//  public function setCreatedTime($timestamp): GoogleCalendarEventInterface;
 
+  /**
+   * Gets the Google Calendar event Start time.
+   *
+   * @return \DateTime
+   *   Creation time of the Google Calendar Event.
+   */
+  public function getStartTime() : \DateTime;
+
+  /**
+   * Sets the Google Calendar event Start time.
+   *
+   * @param \DateTime $start
+   *   The Google Calendar event Start time.
+   *
+   * @return \Drupal\google_calendar\Entity\GoogleCalendarEventInterface
+   *   The called Google Calendar Event entity.
+   */
+//  public function setStartTime(\DateTime $start): GoogleCalendarEventInterface;
+
+  /**
+   * Gets the Google Calendar event End time.
+   *
+   * @return \DateTime
+   *   Creation timestamp of the Google Calendar Event.
+   */
+  public function getEndTime() : \DateTime;
+
+  /**
+   * Sets the Google Calendar Event creation timestamp.
+   *
+   * @param \DateTime $start
+   *   The Google Calendar Event creation timestamp.
+   *
+   * @return \Drupal\google_calendar\Entity\GoogleCalendarEventInterface
+   *   The called Google Calendar Event entity.
+   */
+//  public function setEndTime(\DateTime $start): GoogleCalendarEventInterface;
+
+  /**
+   * Return whether the End Time of the event is meaningful.
+   *
+   * Reflects the Google Calendar "end_time_unspecified" flag.
+   *
+   * @return bool
+   *   Creation timestamp of the Google Calendar Event.
+   *
+   * @see GoogleCalendarEventInterface::getEndTime()
+   */
+  public function isEndTimeSpecified() : bool;
+
+  /**
+   * Define whether the End time of the event is meaningful.
+   *
+   * Reflects the Google Calendar "end_time_unspecified" flag.
+   *
+   * @param bool $specified
+   *   True if this event has a defined end time.
+   *
+   * @return \Drupal\google_calendar\Entity\GoogleCalendarEventInterface
+   *   The called Google Calendar Event entity.
+   */
+//  public function setEndTimeSpecified(bool $specified): GoogleCalendarEventInterface;
+
+  /**
+   * Can guests of the event modify the event itself -- or just the owner?.
+   *
+   * Unpublished Google Calendar Event are only visible to restricted users.
+   *
+   * @return bool
+   *   TRUE if the Google Calendar Event is published.
+   */
+  public function canGuestsModifyEvent(): bool;
+
+  /**
+   * Can guests of the event see who else is invited -- or can only the owner do so?.
+   *
+   * Unpublished Google Calendar Event are only visible to restricted users.
+   *
+   * @return bool
+   *   TRUE if the Google Calendar Event is published.
+   */
+  public function canGuestsSeeInvitees(): bool;
+
+  /**
+   * Is the event Locked at the Google end?
+   *
+   * @return bool
+   *   TRUE if the Google Calendar Event is published.
+   */
+  public function isLocked(): bool;
+  
   /**
    * Returns the Google Calendar Event published status indicator.
    *
@@ -62,7 +171,7 @@ interface GoogleCalendarEventInterface extends ContentEntityInterface, EntityCha
    * @return bool
    *   TRUE if the Google Calendar Event is published.
    */
-  public function isPublished();
+  public function isPublished(): bool;
 
   /**
    * Sets the published status of a Google Calendar Event.
@@ -74,5 +183,29 @@ interface GoogleCalendarEventInterface extends ContentEntityInterface, EntityCha
    *   The called Google Calendar Event entity.
    */
   public function setPublished($published);
+
+  /**
+   * Gets an iCal Identifier for the event.
+   *
+   * @return string
+   *   iCal Identifier of the Google Calendar Event.
+   */
+  public function getGoogleICalId(): string;
+
+  /**
+   * Gets the Google Calendar event Start time.
+   *
+   * @return \Drupal\Core\Url
+   *   URL linking to the event on the Google website.
+   */
+  public function getGoogleLink(): string;
+
+  /**
+   * Gets the Google Calendar ID of the event.
+   *
+   * @return string
+   *   Calendar ID of the event.
+   */
+  public function getGoogleEventId(): string;
 
 }
