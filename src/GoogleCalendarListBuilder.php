34087b178b3d94a27f3b1e3ebbb7ee5da7f5bddd
Ruth Ivimey-Cook <ruth@ivimey.org>
2019-04-16 02:04:39 +0100

    Add sync time & number of events to calendar collection list

--- src/GoogleCalendarListBuilder.php
+++ src/GoogleCalendarListBuilder.php
@@ -21,6 +21,8 @@ class GoogleCalendarListBuilder extends EntityListBuilder {
 
     $header['name'] = $this->t('Name');
     $header['id'] = $this->t('Google Calendar ID');
+    $header['last_sync'] = $this->t('Last synced');
+    $header['number'] = $this->t('Events Loaded');
     return $header + parent::buildHeader();
   }
 
@@ -28,15 +30,25 @@ class GoogleCalendarListBuilder extends EntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
-    /* @var $entity \Drupal\google_calendar\Entity\GoogleCalendar */
+    /** @var $entity \Drupal\google_calendar\Entity\GoogleCalendar */
+    /** @var \Drupal\Core\Entity\EntityStorageInterface $storage */
+    $storage = \Drupal::entityTypeManager()->getStorage('google_calendar_event');
+    /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
+    $query = $storage->getQuery()
+      ->condition('status', 1)
+      ->condition('calendar', $entity->id());
+    $num = $query->count()->execute();
+
+    $last_synced = \Drupal::service('date.formatter')->format($entity->getLastSyncTime(), 'medium');
 
     $row['name'] = Link::createFromRoute(
       $entity->label(),
       'view.google_calendar_events.calendar_list',
       ['google_calendar' => $entity->id()]
     );
-    //$row['name'] = $entity->label();
     $row['id'] = $entity->getGoogleCalendarId();
+    $row['last_sync'] = $last_synced;
+    $row['number'] = $num;
     return $row + parent::buildRow($entity);
   }
 
