33f3f9b5f5e6f896e06446cde43dd856708a518b
Spencer H Brooks <spencer@brooks.digital>
2019-07-11 14:47:34 -0700

    Delete all events when a calendar is deleted

--- src/Form/GoogleCalendarDeleteForm.php
+++ src/Form/GoogleCalendarDeleteForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\google_calendar\Form;
 
 use Drupal\Core\Entity\ContentEntityDeleteForm;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a form for deleting Google Calendar entities.
@@ -11,5 +12,29 @@ use Drupal\Core\Entity\ContentEntityDeleteForm;
  */
 class GoogleCalendarDeleteForm extends ContentEntityDeleteForm {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+    $entity = $this->getEntity();
+    $message = $this->getDeletionMessage();
+
+    // Also delete events associated with this calendar.
+    $eventsToDelete = \Drupal::entityTypeManager()
+      ->getStorage('google_calendar_event')
+      ->loadByProperties(['calendar' => $entity->id()]);
+
+    foreach ($eventsToDelete as $event) {
+      $event->delete();
+    }
+
+    // Finally, delete the parent calendar.
+    $entity->delete();
+    $form_state->setRedirectUrl($this->getRedirectUrl());
+
+    $this->messenger()->addStatus($message);
+    $this->logDeletionMessage();
+  }
 
 }
